202c65199576c8fe9b71d39c1872c98602f9214e
Akash Tyagi <heyakash90@gmail.com>
2025-03-24 12:06:52 +0530

    ..

--- frontend/src/pages/dashboard/User.jsx
+++ frontend/src/pages/dashboard/User.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import CustomComponent from '../../components/userDash/Custom'
 import { Assignment } from '@mui/icons-material'
 import AssignmentSubmit from '../assignments/AssignmentSubmit'
-import ReviewSubmissions from '../assignments/ReviewSubmissions'
+// import ReviewSubmissions from '../assignments/ReviewSubmissions'
 import LiveStream from '../assignments/LiveStream'
 
 const User = () => {
     return (<>
         <CustomComponent />
         <AssignmentSubmit />
-        <ReviewSubmissions/>
+        {/* <ReviewSubmissions/> */}
         <LiveStream/>
         </>
     )
